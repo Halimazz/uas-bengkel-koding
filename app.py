bbe7578e1550588d3c9b1d466742a03b74e4af04
halimazz <sanankciez@gmail.com>
2024-07-14 01:58:16 +0700

    update

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
 from sklearn.preprocessing import StandardScaler
-from imblearn.over_sampling import SMOTE
+from imblearn.over_sampling impipport SMOTE
 from sklearn.model_selection import train_test_split, GridSearchCV
 from sklearn.svm import SVC
 from sklearn.neighbors import KNeighborsClassifier
