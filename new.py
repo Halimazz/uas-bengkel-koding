425c74f8ae4bc6ccb3627073a929406588b0c8ad
halimazz <sanankciez@gmail.com>
2024-07-14 02:23:00 +0700

    Update new.py

--- new.py
+++ new.py
@@ -2,7 +2,6 @@
 import itertools
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
 import streamlit as st
 from sklearn.preprocessing import StandardScaler
 from imblearn.over_sampling import SMOTE
